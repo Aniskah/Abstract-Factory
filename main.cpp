26a3b65761bb77e75bcc5ec15d2dac2f884c3471
Aniskah <eric.ruppert@st.oth-regensburg.de>
2024-12-10 18:28:37 +0100

    Added Abstract Factory

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 //Die Klasse Stuhl definiert das abstrakte Objekt "Stuhl" und die abstrakte Methode "getName"
 class Stuhl {
 public:
-    virtual ~Stuhl(){};
-    virtual std::string getName() const = 0;
+    virtual ~Stuhl(){}; //Abstraktes Object
+    virtual std::string getName() const = 0; //Gibt den Namen des Objekts aus
 };
 
 //Die Klasse AlterStuhl überschreibt die geerbte abstrakten Methode und erbt von Stuhl
@@ -34,8 +34,8 @@ class AntikerStuhl : public Stuhl {
 //Die Klasse Tisch definiert das abstrakte Objekt "Tisch" und die abstrakte Methode "getName"
 class Tisch {
     public:
-    virtual ~Tisch(){};
-    virtual std::string getName() const = 0;
+    virtual ~Tisch(){};//Abstraktes Object
+    virtual std::string getName() const = 0;//Gibt den Namen des Objekts aus
 };
 
 //Die Klasse AlterTisch überschreibt die geerbte abstrakten Methode und erbt von Tisch
@@ -65,8 +65,8 @@ public:
 //Die Klasse Bett definiert das abstrakte Objekt "Bett" und die abstrakte Methode "getName"
 class Bett {
     public:
-    virtual ~Bett(){};
-    virtual std::string getName() const = 0;
+    virtual ~Bett(){};//Abstraktes Object
+    virtual std::string getName() const = 0;//Gibt den Namen des Objekts aus
 };
 
 //Die Klasse AltesBett überschreibt die geerbte abstrakten Methode und erbt von Bett
@@ -104,6 +104,7 @@ class AbstractFactory {
 
 //Die Klasse AlteFactory erbt von AbstractFactory und überschreibt die Methoden um Alte Objekte zu erstellen
 class AlteFactory : public AbstractFactory {
+    //Die Drei abstrakten Methoden der "AbstractFactory" Klasse werden überschrieben
 public:
     Stuhl *createStuhl() const override {
         return new AlterStuhl();
@@ -118,6 +119,7 @@ public:
 
 //Die Klasse NeueFactory erbt von AbstractFactory und überschreibt die Methoden um Neue Objekte zu erstellen
 class NeueFactory : public AbstractFactory {
+    //Die Drei abstrakten Methoden der "AbstractFactory" Klasse werden überschrieben
     public:
     Stuhl *createStuhl() const override {
         return new ModernerStuhl();
@@ -133,6 +135,7 @@ class NeueFactory : public AbstractFactory {
 //Die Klasse AntikeFactory erbt von AbstractFactory und überschreibt die Methoden um Antike Objekte zu erstellen
 class AntikeFactory : public AbstractFactory {
     public:
+    //Die Drei abstrakten Methoden der "AbstractFactory" Klasse werden überschrieben
     Stuhl *createStuhl() const override {
         return new AntikerStuhl();
     }
@@ -146,9 +149,11 @@ class AntikeFactory : public AbstractFactory {
 
 //Der Client übernimmt ein AbstractFactory-Objekt und erstellt die Gefragten Objekte einer Familie
 void Client(const AbstractFactory &factory) {
+    //Die Drei Produkte werden nach der Gewünschten Variante erzeugt
     const Stuhl *produkt_a = factory.createStuhl();
     const Tisch *produkt_b = factory.createTisch();
     const Bett *produkt_c = factory.createBett();
+    //Die "getName" Methode wird auf jedes objekt aufgerufen
     std::cout << produkt_a->getName() << std::endl;
     std::cout << produkt_b->getName() << std::endl;
     std::cout << produkt_c->getName() << std::endl;
@@ -159,15 +164,18 @@ void Client(const AbstractFactory &factory) {
 
 //Die main-Methode erstellt alle 3 Fabriken und gibt die Namen der Objekte gruppiert aus
 int main() {
+    //Gibt Die Objekte der Familie "AlteFactory" aus
     std::cout << "Client: Test für die AlteFactory: " << std::endl;
     AlteFactory *f1 = new AlteFactory();
     Client (*f1);
     delete f1;
     std::cout << std::endl;
+    //Gibt Die Objekte der Familie "NeueFactory" aus
     std::cout << "Client: Test für die NeueFactory: " << std::endl;
     NeueFactory *f2 = new NeueFactory();
     Client(*f2);
     delete f2;
+    //Gibt Die Objekte der Familie "AntikeFactory" aus
     std::cout << std::endl;
     std::cout << "Client: Test für die AntikeFactory: " << std::endl;
     AntikeFactory *f3 = new AntikeFactory();
